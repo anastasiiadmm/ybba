5fdc8664a6e50666a13bf36393a5979931f58776
kadyr <kadyrpoyraz@gmail.com>
2021-08-25 22:43:23 +0600

    YB-115 Fix App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import {NavLink, Route, Switch} from "react-router-dom";
 import './App.css';
+import Login from "./Containers/Login/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+
+            <Switch>
+                <Route path='/' render={() => (
+                    <NavLink to='/login/'>
+                        Login
+                    </NavLink>
+                )} exact/>
+                <Route path='/login' component={Login}/>
+            </Switch>
+
+        </div>
+    );
 }
 
 export default App;
