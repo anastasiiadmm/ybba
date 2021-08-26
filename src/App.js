f0ec3733844fa33ffb75bc6ca535f5bb2686d64a
kadyr <kadyrpoyraz@gmail.com>
2021-08-26 10:44:57 +0600

    YB-115 Added Logout as default component on main page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import {NavLink, Route, Switch} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import './App.css';
 import Login from "./Containers/Login/Login";
+import Logout from "./Containers/Logout/Logout.js";
+import PrivateRoute from "./HOC/PrivatRoute/PrivatRoute.js";
 
 const App = () => {
     return (
         <div>
 
             <Switch>
-                <Route path='/' render={() => (
-                    <NavLink to='/login/'>
-                        Login
-                    </NavLink>
-                )} exact/>
+                <PrivateRoute path='/' component={Logout} exact/>
                 <Route path='/login' component={Login}/>
             </Switch>
 
