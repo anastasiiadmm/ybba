025be73572ee12b4a0b660efd2317a5a2695b5ec
kadyr <kadyrpoyraz@gmail.com>
2021-08-31 22:19:03 +0600

    Eslint fix #2

--- src/Containers/Registration/RegistrationBaseBlock/RegistrationBaseBlock.js
+++ src/Containers/Registration/RegistrationBaseBlock/RegistrationBaseBlock.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import PropTypes from "prop-types";
-import {NavLink} from "react-router-dom";
+import PropTypes from 'prop-types';
+import {NavLink} from 'react-router-dom';
 
 
 const RegistrationBaseBlock = props => {
@@ -9,16 +9,16 @@ const RegistrationBaseBlock = props => {
     const {children, onSubmit} = props
 
     return (
-        <div className="all-page">
-            <div className="form__wrap">
-                <div className="form form_narrow">
+        <div className='all-page'>
+            <div className='form__wrap'>
+                <div className='form form_narrow'>
                     <form onSubmit={onSubmit}>
                         {children}
-                        <p className="form__link-wrap">
+                        <p className='form__link-wrap'>
                             Уже есть аккаунт?{' '}
-                            <NavLink className="form__link" to='/login/'>Войти</NavLink>
+                            <NavLink className='form__link' to='/login/'>Войти</NavLink>
                         </p>
-                        <p className="form__agreement">
+                        <p className='form__agreement'>
                             Регистрируясь в сервисе, принимаю условия <NavLink to='#'>соглашения</NavLink>{' '}
                             и <NavLink to='/policy'>политики конфидециальности</NavLink>
                         </p>
