33b6c70e99986d4afe759963db59a62863e83f6b
kadyr <kadyrpoyraz@gmail.com>
2021-08-31 10:52:31 +0600

    YB-125 Added component for icons

---
+++ src/Components/Icon/Icon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import IcoMoon from "react-icomoon";
+import iconSet from '../../assets/vendor/icomoon/fonts/selection.json'
+
+const Icon = (props) => {
+    return <IcoMoon iconSet={iconSet} {...props}/>
+}
+
+export default Icon;
