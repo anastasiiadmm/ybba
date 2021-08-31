56158767d5aec14faf2e286f8497ef4bfe855414
kadyr <kadyrpoyraz@gmail.com>
2021-08-31 15:49:07 +0600

    YB-137 Added Side bar component

---
+++ src/Components/SideBar/SideBarLink/SideBarLink.js
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types";
+import React from 'react';
+import {useHistory} from "react-router";
+import {Link} from "react-router-dom";
+import {addClasses} from "../../../utils/addClasses/addClasses.js";
+
+const SideBarLink = (props) => {
+
+    const {children, to, classes} = props
+
+    const history = useHistory()
+    const {pathname} = history.location
+
+    const isActive = pathname === to
+
+    return (
+        <li className="main-nav__item">
+            <Link
+                to={to}
+                className={addClasses(`main-nav__link ${classes}`, {
+                    'active': isActive
+                })}
+            >
+                {children}
+            </Link>
+        </li>
+    );
+}
+
+SideBarLink.propTypes = {
+    to: PropTypes.string.isRequired,
+    classes: PropTypes.string
+}
+
+export default SideBarLink;
