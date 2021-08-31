2405029170a289e97b3ef8367cd0e202d7079e2c
kadyr <kadyrpoyraz@gmail.com>
2021-08-31 10:47:14 +0600

    YB-125 Added request interceptor in axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,23 @@
 import config from "./config";
+import {store} from "./index.js";
 
 const axios = require("axios");
 
 
 const axiosApi = axios.create({
     baseURL: config.apiUrl + '/api/v1/'
+})
+
+axiosApi.interceptors.request.use(async config => {
+    const key = store.getState()?.user?.tokens?.access
+    console.log(key)
+    if (key) {
+        config.headers = {
+            ...config.headers,
+            "Authorization": `Bearer ${key}`,
+        }
+    }
+    return config
 })
 
 export default axiosApi
