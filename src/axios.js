8a5f0fc2771b9c31fcea6739a7e8a080bdcb2483
kadyr <kadyrpoyraz@gmail.com>
2021-09-01 13:56:11 +0600

    HotFix: Исправил url в axios

--- src/axios.js
+++ src/axios.js
@@ -5,7 +5,7 @@ import {store} from './index.js';
 const axios = require('axios');
 
 const axiosApi = axios.create({
-    baseURL: config.apiUrl + '/api/v1/'
+    baseURL: config.apiUrl
 })
 
 axiosApi.interceptors.request.use(async config => {
