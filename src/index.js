deab170d0d65589fbd7bbb75d88cb5d83d8eead1
kadyr <kadyrpoyraz@gmail.com>
2021-09-06 21:10:03 +0600

    YB-146 Fixed imports

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import {Provider} from 'react-redux'
-
 import {BrowserRouter} from 'react-router-dom';
 import configureStore from './redux/store.js'
 import reportWebVitals from './reportWebVitals';
-import App from './App';
 import {initSentry} from './sentry/sentry';
+import App from './App';
 
 import './assets/fonts/icomoon/icomoon.css'
 import './assets/vendor/slimselect/slimselect.min.css'
