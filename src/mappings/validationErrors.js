626aaa2125782e69d7bce86a6a98850a9965ecbf
kadyr <kadyrpoyraz@gmail.com>
2021-09-06 21:11:30 +0600

    YB-146 Added mapping for validation errors

---
+++ src/mappings/validationErrors.js
@@ -0,0 +1,5 @@
+export const validationMessagesMapping = {
+    'This password is too short. It must contain at least 8 characters.': 'Слишком короткий пароль. Пароль должен содержать больше 8 символов',
+    'This password is too common.': 'Слишком простой пароль',
+    'This password is entirely numeric.': 'Этот пароль полностью числовой.'
+}
