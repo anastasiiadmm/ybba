fd59c32fb2e037228c557d59662334042b676ef8
kadyr <kadyrpoyraz@gmail.com>
2021-09-16 12:56:27 +0600

    YB-157 Added 2 new reducers into redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,8 @@ import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit'
 
 import {loadFromLocalStorage, localStorageMiddleware} from './utils/localstorage.js';
 import userReducer from './user/userSlice'
+import authReducer from './auth/authSlice'
+import childReducer from './child/childSlice'
 
 
 const middlewares = [localStorageMiddleware]
@@ -10,6 +12,8 @@ const configureStoreCreator = () => {
     return configureStore({
         reducer: {
             user: userReducer,
+            auth: authReducer,
+            child: childReducer
         },
         preloadedState: loadFromLocalStorage(),
         middleware: [...getDefaultMiddleware(), ...middlewares]
