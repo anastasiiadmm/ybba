08d6d5542355f0b5ee1834ad7856d994f05ad4c9
kadyr <kadyrpoyraz@gmail.com>
2021-09-16 12:55:04 +0600

    YB-157 Hide excess sidebar menus

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -19,24 +19,24 @@ const SideBar = () => {
                     <SideBarLink to='/' classes='main-nav__link_home'>
                         Главная
                     </SideBarLink>
-                    <SideBarLink to='/home-work/' classes='main-nav__link_hw'>
-                        Домашнее задание
-                    </SideBarLink>
+                    {/*<SideBarLink to='/home-work/' classes='main-nav__link_hw'>*/}
+                    {/*    Домашнее задание*/}
+                    {/*</SideBarLink>*/}
                     <SideBarLink to='/profile/' classes='main-nav__link_lk'>
                         Личный кабинет
                     </SideBarLink>
-                    <SideBarLink to='/child-profile/' classes='main-nav__link_profile'>
-                        Профиль ребёнка
-                    </SideBarLink>
-                    <SideBarLink to='/payments/' classes='main-nav__link_payment'>
-                        Управление платежами
-                    </SideBarLink>
-                    <SideBarLink to='/settings/' classes='main-nav__link_setting'>
-                        Настройки
-                    </SideBarLink>
-                    <SideBarLink to='/video-test/' classes='main-nav__link_test'>
-                        Тест видеосвязи
-                    </SideBarLink>
+                    {/*<SideBarLink to='/child-profile/' classes='main-nav__link_profile'>*/}
+                    {/*    Профиль ребёнка*/}
+                    {/*</SideBarLink>*/}
+                    {/*<SideBarLink to='/payments/' classes='main-nav__link_payment'>*/}
+                    {/*    Управление платежами*/}
+                    {/*</SideBarLink>*/}
+                    {/*<SideBarLink to='/settings/' classes='main-nav__link_setting'>*/}
+                    {/*    Настройки*/}
+                    {/*</SideBarLink>*/}
+                    {/*<SideBarLink to='/video-test/' classes='main-nav__link_test'>*/}
+                    {/*    Тест видеосвязи*/}
+                    {/*</SideBarLink>*/}
                 </ul>
                 <Logout/>
             </nav>
