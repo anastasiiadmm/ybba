4dc6582d4a33a2b7d04a2c2e970764981dbd8363
kadyr <kadyrpoyraz@gmail.com>
2021-09-16 12:16:29 +0600

    YB-157 Added interceptor for responses

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-import config from './config';
+import config, {unauthorized401} from './config';
 import {store} from './index.js';
+import {refreshToken} from './redux/auth/authSlice';
 
 
 const axiosApi = axios.create({
@@ -9,7 +10,7 @@ const axiosApi = axios.create({
 })
 
 axiosApi.interceptors.request.use(async config => {
-    const key = store.getState()?.user?.tokens?.access
+    const key = store.getState()?.auth?.tokens?.access
     if (key) {
         config.headers = {
             ...config.headers,
@@ -17,6 +18,27 @@ axiosApi.interceptors.request.use(async config => {
         }
     }
     return config
+})
+
+axiosApi.interceptors.response.use(async config => {
+    return config
+}, async error => {
+    const originalRequest = error.config;
+
+    const statusCode = error?.response?.status
+    const user = store.getState().auth.user
+
+    if (user && (!statusCode || statusCode === unauthorized401)) {
+        originalRequest._retry = true
+        await store.dispatch(refreshToken())
+
+        const newAccessToken = store.getState().auth.tokens.access
+        axiosApi.defaults.headers['Authorization'] = `Bearer ${newAccessToken()}`
+
+        return axiosApi(originalRequest)
+    }
+
+    return Promise.reject(error)
 })
 
 export default axiosApi
