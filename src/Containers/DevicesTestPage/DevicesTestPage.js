c23cfeb39f558761249c36192bcf4980a9e8992a
kadyr <kadyrpoyraz@gmail.com>
2021-09-17 14:00:47 +0600

    YB-180 Added DevicesTestPage.js container

---
+++ src/Containers/DevicesTestPage/DevicesTestPage.js
@@ -0,0 +1,45 @@
+import React, {useRef, useEffect, useState} from 'react';
+
+import {v4 as uuidv4} from 'uuid'
+
+import SidebarContainer from '../../Components/SidebarContainer/SidebarContainer';
+import Jitsi from '../../Components/Jitsi/Jitsi';
+import {jitsiTools} from '../../constants';
+
+
+const DevicesTestPage = () => {
+
+    const jitsiWrapper = useRef(null)
+    const [jitsiWindowHeight, setJitsiWindowHeight] = useState(0)
+
+    const tools = [
+        jitsiTools.mic,
+        jitsiTools.camera
+    ]
+
+    const randomMeetingId = uuidv4()
+
+    useEffect(() => {
+        console.log(jitsiWrapper.current.offsetHeight)
+        if (jitsiWrapper.current) {
+            setJitsiWindowHeight(jitsiWrapper.current.offsetHeight)
+        }
+    }, [])
+
+    return (
+        <SidebarContainer>
+            <div className='main__inner' ref={jitsiWrapper}>
+                {jitsiWindowHeight && (
+                    <Jitsi
+                        meetingId={randomMeetingId}
+                        height={jitsiWindowHeight}
+                        width='100%'
+                        toolbarItems={tools}
+                    />
+                )}
+            </div>
+        </SidebarContainer>
+    );
+}
+
+export default DevicesTestPage;
