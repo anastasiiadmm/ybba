8c1beea8b74e4c4d8725e3e327e7ba512a9aaf31
kadyr <kadyrpoyraz@gmail.com>
2021-09-17 13:41:19 +0600

    YB-180 Added mapping with jitsi tools in meet

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,8 @@ export const GAME_FILE_TYPE_LOADER = 'loader'
 export const GAME_FILE_TYPE_DATA = 'data'
 export const GAME_FILE_TYPE_FRAMEWORK = 'framework'
 export const GAME_FILE_TYPE_WASM = 'wasm'
+
+export const jitsiTools = {
+    mic: 'microphone',
+    camera: 'camera'
+}
