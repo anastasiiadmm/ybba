a46a480e1d3d500cea3f82e3a8c9c3ea7d6988d0
kadyr <kadyrpoyraz@gmail.com>
2021-09-21 09:50:43 +0600

    YB-155 Added handlers for starting lesson, changing active game in lesson and finishing lesson

--- src/redux/lesson/lessonSlice.js
+++ src/redux/lesson/lessonSlice.js
@@ -1,20 +1,41 @@
 import {createSlice} from '@reduxjs/toolkit';
 
+import {GET_LESSON, GET_LESSON_ERROR, CHANGE_ACTIVE_GAME, CHANGE_LESSON_STATUS} from '../actionTypes';
+import {LESSON_STATUS_FINISHED} from '../../constants';
+
 
 const nameSpace = 'lesson'
 
 const INITIAL_STATE = {
-    activeGame: null
+    activeGame: null,
+    lesson: null,
+    error: null
 }
 
 const lessonSlice = createSlice({
     name: nameSpace,
     initialState: INITIAL_STATE,
     reducers: { },
-    extraReducers: { },
+    extraReducers: {
+        [GET_LESSON]: (state, {payload}) => {
+            state.lesson = payload.lesson
+        },
+        [GET_LESSON_ERROR]: (state, {payload}) => {
+            state.error = payload.message
+        },
+
+        [CHANGE_ACTIVE_GAME]: (state, {payload}) => {
+            state.lesson = payload.lesson
+        },
+
+        [CHANGE_LESSON_STATUS]: (state, {payload}) => {
+            if (payload.status === LESSON_STATUS_FINISHED) {
+                state.lesson = null
+            }
+        }
+    },
 })
 
 
-export const {} = lessonSlice.actions
 export const lessonSelector = state => state.lesson
 export default lessonSlice.reducer
