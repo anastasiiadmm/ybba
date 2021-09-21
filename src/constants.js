3a54d14344592022efa7a6dab6027bbd529967b4
kadyr <kadyrpoyraz@gmail.com>
2021-09-21 09:48:26 +0600

    YB-155 Added constants for lesson statuses

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,9 @@
-export const GAME_FILE_TYPE_LOADER = 'loader'
-export const GAME_FILE_TYPE_DATA = 'data'
-export const GAME_FILE_TYPE_FRAMEWORK = 'framework'
-export const GAME_FILE_TYPE_WASM = 'wasm'
+export const GAME_FILE_TYPE_LOADER = 'file_loader'
+export const GAME_FILE_TYPE_DATA = 'file_data'
+export const GAME_FILE_TYPE_FRAMEWORK = 'file_framework'
+export const GAME_FILE_TYPE_WASM = 'file_wasm'
+
+export const LESSON_STATUS_NEW = 'new'
+export const LESSON_STATUS_PENDING = 'pending'
+export const LESSON_STATUS_ON = 'process'
+export const LESSON_STATUS_FINISHED = 'finished'
