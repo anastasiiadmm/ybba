40b34ec58d88f82e06c04940ea5fb96e624467c2
kadyr <kadyrpoyraz@gmail.com>
2021-09-23 10:48:36 +0600

    YB-155 Added handlers for game buttons and timer

--- src/Containers/Lesson/GameSidebar/GameSidebar.js
+++ src/Containers/Lesson/GameSidebar/GameSidebar.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 
 import PropTypes from 'prop-types';
 
 import WebCam from '../../../Components/WebCam/WebCam';
 import Button from '../../../Components/Button/Button';
 import {addClasses} from '../../../utils/addClasses/addClasses';
+import {LessonContext} from '../../../context/LessonContext/LessonContext';
+import {gameActions, userRoles} from '../../../constants';
+import {checkUserRole} from '../../../utils/user';
+import Timer from '../../../Components/Timer/Timer';
 
 import './gameSidebar.css';
 
@@ -18,6 +22,9 @@ const GameSidebar = (props) => {
     const games = lesson?.games
     const activeGame = lesson?.active_game_id
 
+    const [timerData, setTimerData] = useState(null)
+    const {triggerGameAction} = useContext(LessonContext)
+
     const gameClickHandler = game => {
         gameOnClick(game)
     }
@@ -25,43 +32,80 @@ const GameSidebar = (props) => {
         onFinishLesson()
     }
 
+    const triggerPrevAction = () => triggerGameAction(gameActions.PREV_ACTION)
+    const triggerStartGame = () => triggerGameAction('')
+    const triggerPauseGame = () => triggerGameAction(gameActions.PAUSE_GAME)
+    const triggerGameRestart = () => triggerGameAction(gameActions.RESTART_GAME)
+    const triggerNextAction = () => triggerGameAction(gameActions.NEXT_ACTION)
+
+
+    useEffect(() => {
+        const now = new Date()
+        setTimerData({
+            from: new Date(
+                now.getFullYear(),
+                now.getMonth(),
+                now.getDate(),
+                1,
+                30,
+                0
+            ),
+            to: new Date(
+                now.getFullYear(),
+                now.getMonth(),
+                now.getDate(),
+                23,
+                10,
+                0
+            )
+        })
+    }, [])
+
     return (
         <>
-            <WebCam meetingId='123123' onClick={webcamOnClick}/>
-            <div className='game__controls'>
-                <Button className='btn-control btn-control_back' />
-                <Button className='btn-control btn-control_play' />
-                <Button className='btn-control btn-control_pause' />
-                <Button className='btn-control btn-control_restart' />
-                <Button className='btn-control btn-control_forward' />
-            </div>
-            <div className='game__progress'>
-                <div className='game__progress-line'>
-                    <span className='game__progress-point' style={{width: '50%'}}/>
+            {lesson?.id && <WebCam meetingId={lesson.id} onClick={webcamOnClick}/>}
+            {checkUserRole(userRoles.therapist) && (
+                <div className='game__controls'>
+                    <Button className='btn-control btn-control_back gameActionButton' onClick={triggerPrevAction} />
+                    <Button className='btn-control btn-control_play gameActionButton' onClick={triggerStartGame} />
+                    <Button className='btn-control btn-control_pause gameActionButton' onClick={triggerPauseGame} />
+                    <Button className='btn-control btn-control_restart gameActionButton' onClick={triggerGameRestart} />
+                    <Button className='btn-control btn-control_forward gameActionButton' onClick={triggerNextAction} />
                 </div>
-                <div className='game__progress-time'>18:00</div>
-            </div>
-            <div className='game__info d-flex flex-column'>
-                <h5 className='game__info-title'>Название занятия</h5>
-                <ol className='game__lesson-list'>
-                    {games && games.map(game => {
-                        return <li
-                            className={addClasses('game__lesson-item gameListItem', {
-                                'activeGame': game.id === activeGame
-                            })}
-                            onClick={() => gameClickHandler(game)}
+            )}
+            {timerData && (
+                <Timer
+                    from={timerData.from}
+                    to={timerData.to}
+                />
+            )}
+            {checkUserRole(userRoles.therapist) && (
+                <div className='game__info d-flex flex-column h-100'>
+                    <h5 className='game__info-title'>Список игр:</h5>
+                    <ol className='game__lesson-list'>
+                        {games && games.map(game => {
+                            return <li
+                                className={addClasses('game__lesson-item gameListItem', {
+                                    'activeGame': game.id === activeGame
+                                })}
+                                onClick={() => gameClickHandler(game)}
+                            >
+                                {game.display_name}
+                            </li>
+                        })}
+                    </ol>
+                    <div
+                        className='game__info-bottom mt-auto finishLessonArea'
+                        onClick={finishLessonClickHandler}
+                    >
+                        <button
+                            className='btn-complete' type='button'
                         >
-                            {game.display_name}
-                        </li>
-                    })}
-                </ol>
-                <div
-                    className='game__info-bottom mt-auto'
-                    onClick={finishLessonClickHandler}
-                >
-                    <button className='btn-complete' type='button'>Завершить занятие</button>
+                            Завершить занятие
+                        </button>
+                    </div>
                 </div>
-            </div>
+            )}
         </>
     );
 }
