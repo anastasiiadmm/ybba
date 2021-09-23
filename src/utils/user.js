d4051e00710a35bf1e64b25fcd71d2084f285e1a
kadyr <kadyrpoyraz@gmail.com>
2021-09-23 10:55:41 +0600

    YB-155 Added function to check user role

--- src/utils/user.js
+++ src/utils/user.js
@@ -4,3 +4,8 @@ export const isLogin = () => {
     const user = store.getState().auth.user
     return !!user
 }
+
+export const checkUserRole = role => {
+    const user = store.getState().auth.user
+    return user.role === role
+}
