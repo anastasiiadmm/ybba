89f81cebfe2c4df6c6d2028948f316e466fddade
kadyr <kadyrpoyraz@gmail.com>
2021-09-23 10:33:17 +0600

    YB-155 Added check which will hide block with notes if user is parent

--- src/Containers/Lesson/GameMain/GameMain.js
+++ src/Containers/Lesson/GameMain/GameMain.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import GameContainer from '../../GameContainer/GameContainer';
 import Button from '../../../Components/Button/Button';
+import {checkUserRole} from '../../../utils/user';
+import {userRoles} from '../../../constants';
 
 import './gameMain.css'
 
@@ -15,19 +17,21 @@ const GameMain = () => {
                     <GameContainer/>
                 </div>
             </div>
-            <div className='notes game__notes'>
-                <div className='notes__top'>
-                    <span className='notes__title'>Заметки</span>
-                    <Button className='notes__link border-0 bg-transparent'>Протокол обследования</Button>
-                    <Button className='notes__link border-0 bg-transparent'>Речевая карта</Button>
+            {checkUserRole(userRoles.therapist) && (
+                <div className='notes game__notes'>
+                    <div className='notes__top'>
+                        <span className='notes__title'>Заметки</span>
+                        <Button className='notes__link border-0 bg-transparent'>Протокол обследования</Button>
+                        <Button className='notes__link border-0 bg-transparent'>Речевая карта</Button>
+                    </div>
+                    <div className='notes__field'>
+                        <form action='#'>
+                            <textarea className='notes__area'/>
+                            <Button className='notes__btn' type='submit'/>
+                        </form>
+                    </div>
                 </div>
-                <div className='notes__field'>
-                    <form action='#'>
-                        <textarea className='notes__area'/>
-                        <Button className='notes__btn' type='submit'/>
-                    </form>
-                </div>
-            </div>
+            )}
         </>
     );
 }
