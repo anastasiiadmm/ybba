aac9233a741a5f76325be358261430ec82ae51f8
kadyr <kadyrpoyraz@gmail.com>
2021-09-28 16:35:08 +0600

    YB-199 Added new logic for MainDashboard.js

--- src/Containers/MainDashboard/MainDashboard.js
+++ src/Containers/MainDashboard/MainDashboard.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import moment from 'moment';
 
 import Balance from './Balance/Balance.js';
 import ChildActivity from './ChildActivity/ChildActivity.js';
@@ -9,24 +10,47 @@ import MainTitleBlock from './MainTitleBlock/MainTitleBlock.js';
 import NextLessons from './NextLessons/NextLessons.js';
 import Timetable from './Timetable/Timetable.js';
 import SidebarContainer from '../../Components/SidebarContainer/SidebarContainer';
-import {getTimesDay, getNowDate} from '../../utils/date/dateUtils';
+import {getTimesDay, getNowDate, strDateToMoment} from '../../utils/date/dateUtils';
 import {authSelector} from '../../redux/auth/authSlice';
+import UpcomingLessonBlock from './UpcomingLessonBlock/UpcomingLessonBlock';
+import {getLessons, dashBoardSelector} from '../../redux/dashBoard/dashBoardSlice';
+import {lessonStatuses} from '../../constants';
 
 
 const MainDashboard = () => {
 
     const {user} = useSelector(authSelector)
+    const {lessons} = useSelector(dashBoardSelector)
 
     const [nowDate, setNowDate] = useState()
+    const [closesLesson, setClosesLesson] = useState(null)
+
+    const dispatch = useDispatch()
 
     const partOfDay = getTimesDay()
 
     useEffect(() => {
+        if (lessons) {
+            const now = moment()
+            const lesson = lessons.filter(function (lesson) {
+                return (
+                    strDateToMoment(lesson.time_slot.day.date) - now < 0 &&
+                    lesson.status === lessonStatuses.new
+                );
+            })
+            setClosesLesson(lesson[0])
+        }
+    }, [lessons])
+
+    useEffect(() => {
+        const data = {userId: user.id}
+        dispatch(getLessons(data))
+
         setNowDate(getNowDate())
         setInterval(() => {
             setNowDate(getNowDate())
         }, 1000)
-    }, [])
+    }, [dispatch, user])
 
     return (
         <SidebarContainer>
@@ -39,6 +63,17 @@ const MainDashboard = () => {
                     />
                     <div className='content'>
                         <div className='content__inner'>
+
+                            <div className='content__row'>
+                                <div className='content__col'>
+                                    {closesLesson && (
+                                        <UpcomingLessonBlock
+                                            lesson={closesLesson}
+                                        />
+                                    )}
+                                </div>
+                            </div>
+
                             <div className='content__row'>
                                 <div className='content__col content__col_w60'>
                                     <NextLessons/>
