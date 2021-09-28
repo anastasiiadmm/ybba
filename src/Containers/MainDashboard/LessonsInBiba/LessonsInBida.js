65504be93261860a0a8384b5fb853e3e2dfaa423
kadyr <kadyrpoyraz@gmail.com>
2021-09-28 16:33:32 +0600

    YB-199 Added showing of count of all finished lessons of children

--- src/Containers/MainDashboard/LessonsInBiba/LessonsInBida.js
+++ src/Containers/MainDashboard/LessonsInBiba/LessonsInBida.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useSelector} from 'react-redux';
+
+import {dashBoardSelector} from '../../../redux/dashBoard/dashBoardSlice';
+import {lessonStatuses} from '../../../constants';
 
 
 const LessonsInBiba = () => {
+
+    const [finishedLessons, setFinishedLessons] = useState(null)
+
+    const {lessons} = useSelector(dashBoardSelector)
+
+    useEffect(() => {
+        if (lessons) {
+            setFinishedLessons(
+                lessons.filter(lesson => lesson.status === lessonStatuses.finished)
+            )
+        }
+    }, [lessons])
+
     return (
         <div className='content__col content__col_w50'>
             <div className='info-item info-item_md'>
                 <h5 className='info-item__title'>Занятий в Бибе</h5>
+                {finishedLessons && <div className='info-item__count-lesson'>{finishedLessons.length}</div>}
             </div>
         </div>
     );
