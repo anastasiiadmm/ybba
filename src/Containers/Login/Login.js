1b50a780fb622439d2f3bd763e4f30dfc9a26bb9
kadyr <kadyrpoyraz@gmail.com>
2021-10-15 11:16:52 +0600

    YB-226 Fixed weight of font

--- src/Containers/Login/Login.js
+++ src/Containers/Login/Login.js
@@ -53,7 +53,7 @@ const Login = () => {
             <div className='form__wrap'>
                 <div className='form form_narrow'>
                     <form onSubmit={submitChangeHandler}>
-                        <h4 className='form__title'>Войти</h4>
+                        <h4 className='form__title'><b>Войти</b></h4>
                         {/*<div className='form__row'>*/}
                         {/*    <p className='form__text'>Через соцсеть</p>*/}
                         {/*    <SocialMediaBlock/>*/}
