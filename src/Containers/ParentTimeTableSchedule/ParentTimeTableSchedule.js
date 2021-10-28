a0dcac2a55508e6a1f0908b677e0549e444aa83d
anastasiiapanama <kilma.1334@gmail.com>
2021-10-28 10:25:38 +0600

    YB-250 added time sampling for ParentTimeTableSchedule.js component

--- src/Containers/ParentTimeTableSchedule/ParentTimeTableSchedule.js
+++ src/Containers/ParentTimeTableSchedule/ParentTimeTableSchedule.js
@@ -8,8 +8,6 @@ import {getNowDate, strDateToMoment} from "../../utils/date/dateUtils";
 import {useDispatch, useSelector} from "react-redux";
 import {authSelector} from "../../redux/auth/authSlice";
 import Button from "../../Components/Button/Button";
-import {namesOfDaysOfWorkWeekShort} from "../../constants";
-import {addClasses} from "../../utils/addClasses/addClasses";
 import {generateDateRange} from "../MainDashboard/Calendar/utils";
 
 const ParentTimeTableSchedule = () => {
@@ -23,6 +21,11 @@ const ParentTimeTableSchedule = () => {
     const [currentDate, setCurrentDate] = useState(moment())
     const [dates, setDates] = useState(null)
     const [activeDate, setActiveDate] = useState(moment())
+
+    const startOfWeek = currentDate.clone().startOf('week')
+    const endOfWeek = currentDate.clone().endOf('week')
+    const startOfCalendar = startOfWeek.clone().subtract(startOfWeek.day() - 1, 'days')
+    const endOfCalendar = endOfWeek.clone().subtract(2 - endOfWeek.day(), 'days')
 
     useEffect(() => {
         const data = {userId: user.id}
@@ -43,25 +46,20 @@ const ParentTimeTableSchedule = () => {
 
     useEffect(() => {
         if (currentDate) {
-            const startOfMont = currentDate.clone().startOf('month')
-            const endOfMonth = currentDate.clone().endOf('month')
-            const startOfCalendar = startOfMont.clone().subtract(startOfMont.day() - 1, 'days')
-            const endOfCalendar = endOfMonth.clone().add(7 - endOfMonth.day(), 'days')
-
             setDates(
                 generateDateRange(startOfCalendar.clone(), endOfCalendar.clone())
             )
         }
     }, [currentDate])
 
-    const toNextMonth = () => {
+    const toNextWeek = () => {
         setCurrentDate(
-            currentDate.clone().add(1, 'month')
+            currentDate.clone().add(1, 'week')
         )
     }
-    const toPrevMonth = () => {
+    const toPrevWeek = () => {
         setCurrentDate(
-            currentDate.clone().subtract(1, 'month')
+            currentDate.clone().subtract(1, 'week')
         )
     }
 
@@ -84,65 +82,46 @@ const ParentTimeTableSchedule = () => {
                         <div className="calendar-big__top">
                             <Button to='/' className="calendar-big__close" />
                             <div className="calendar-big__label">Период</div>
-                            <input className="calendar-big__field field" type="text" value="09 Авг - 13 Авг" />
+                            <input className="calendar-big__field field" type="text"
+                                   value={`${startOfCalendar.format("DD MMM")} - ${endOfCalendar.format("DD MMM")}`}
+                            />
                         </div>
                         <div className="calendar-big__btns">
                             <button type="button" className="calendar-big__prev"
-                                    onClick={toPrevMonth}/>
+                                    onClick={toPrevWeek}
+                            />
 
                             <button type="button" className="calendar-big__next"
-                                    onClick={toNextMonth}/>
+                                    onClick={toNextWeek}
+                            />
                         </div>
                         <div className="calendar-big__body">
                             <div className="calendar-big__row">
                                 <div className="calendar-big__time" />
-                                {namesOfDaysOfWorkWeekShort.map((dayOfWeek, idx) => {
-                                    return <div key={idx} className="calendar-big__col">{dayOfWeek}</div>
-                                })}
                                 {dates && dates.map((date, idx) => {
-                                    console.log(date)
-
-                                    const lessonsForDay = getLessonsForDate(date)
-
                                     return (
-                                        <span
-                                            key={idx}
-                                            className={addClasses('datepick__cell', {
-                                                'prev': !date.isSame(currentDate, 'month'),
-                                                'violet selected': activeDate.isSame(date, 'date'),
-                                                'isLesson': !!lessonsForDay?.length
-                                            })}
-                                            onClick={() => dateOnClick(date)}
-                                        >
-                                                {date.date()}
-                                            </span>
+                                        <div className="calendar-big__col" key={idx}>
+                                            {date.format('DD, ddd')}
+                                        </div>
                                     )
                                 })}
                             </div>
                             <div className="calendar-big__wrap scroll-block">
                                 <div className="calendar-big__row">
                                     <div className="calendar-big__time">07:00</div>
-                                    <div className="calendar-big__col"></div>
+                                    <div className="calendar-big__col" />
                                     <div className="calendar-big__col isLesson">
                                         <span className="isLesson__icon violet">Дг</span>
                                         <p className="isLesson__title">Диагностическое занятие</p>
                                         <span className="isLesson__time">07:00</span>
                                     </div>
-                                    <div className="calendar-big__col"></div>
-                                    <div className="calendar-big__col"></div>
+                                    <div className="calendar-big__col" />
+                                    <div className="calendar-big__col" />
                                     <div className="calendar-big__col isLesson">
                                         <span className="isLesson__icon pink">Дг</span>
                                         <p className="isLesson__title">Диагностическое занятие</p>
                                         <span className="isLesson__time">07:00</span>
                                     </div>
-                                </div>
-                                <div className="calendar-big__row">
-                                    <div className="calendar-big__time">07:30</div>
-                                    <div className="calendar-big__col"></div>
-                                    <div className="calendar-big__col"></div>
-                                    <div className="calendar-big__col"></div>
-                                    <div className="calendar-big__col"></div>
-                                    <div className="calendar-big__col"></div>
                                 </div>
                             </div>
                         </div>
