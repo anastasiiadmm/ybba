54d7a7034faab05f3f60b290baf6ffbbbe8eea36
kadyr <kadyrpoyraz@gmail.com>
2021-11-09 14:07:24 +0600

    YB-293 Added component for dragging blocks

---
+++ src/Components/Drag/Drag.js
@@ -0,0 +1,29 @@
+import React, { useRef } from 'react';
+
+import { useDraggable } from 'hooks/useDraggable/useDraggable.js';
+
+import 'Components/Drag/drag.css'
+
+
+const Drag = (props) => {
+
+    const {
+        children
+    } = props
+
+    const jitsiRef = useRef(null)
+    useDraggable(jitsiRef)
+
+    return (
+        <div
+
+            className='position-absolute'
+            style={{ right: 0 }}
+            ref={jitsiRef}
+        >
+            {children}
+        </div>
+    );
+}
+
+export default Drag;
