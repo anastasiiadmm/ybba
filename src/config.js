ad5da7cbe775ab48bb7bbfbf838d8402e17bce22
kadyr <kadyrpoyraz@gmail.com>
2021-11-09 15:50:41 +0600

    YB-293 Fixed configs

--- src/config.js
+++ src/config.js
@@ -1,20 +1,19 @@
+import { apiUrls, wsUrls } from 'constants.js';
+
 
 const {
-    REACT_APP_API_URL, REACT_APP_SENTRY_DSN, REACT_APP_ENVIRONMENT, REACT_APP_WS_URL,
-    REACT_APP_ROBOKASSA_MERCHANT_LOGIN, REACT_APP_ROBOKASSA_PASSWORD
+    REACT_APP_SENTRY_DSN, REACT_APP_ENVIRONMENT, REACT_APP_ROBOKASSA_MERCHANT_LOGIN, REACT_APP_ROBOKASSA_PASSWORD
 } = process.env
 
-const apiUrl = REACT_APP_API_URL || 'http://localhost:8000'
-const frontendUrl = REACT_APP_API_URL || 'http://localhost:3000'
-const wsUrl = REACT_APP_WS_URL || 'ws://localhost:8000/ws'
-const sentryDSN = REACT_APP_SENTRY_DSN || ''
 const appEnvironment = REACT_APP_ENVIRONMENT || 'local'
+const apiUrl = apiUrls[appEnvironment]
+const wsUrl = wsUrls[appEnvironment]
+const sentryDSN = REACT_APP_SENTRY_DSN || ''
 const robocassa_merchant_login = REACT_APP_ROBOKASSA_MERCHANT_LOGIN || null
 const robocassa_password = REACT_APP_ROBOKASSA_PASSWORD || null
 
 const config = {
     apiUrl,
-    frontendUrl,
     wsUrl,
     sentryDSN,
     appEnvironment,
@@ -29,6 +28,6 @@ const config = {
 
 export const unauthorized401 = 401
 
-export const defaultError = {detail: 'На сервере что-то пошло не так'}
+export const defaultError = { detail: 'На сервере что-то пошло не так' }
 
 export default config;
