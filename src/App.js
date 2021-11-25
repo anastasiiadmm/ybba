9580c4feb73be30cfdbd6707b87fc69b0f3c6120
kadyr <kadyrpoyraz@gmail.com>
2021-11-25 12:07:51 +0600

    YB-338 Added base registration base

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 import Page404 from 'Components/Page404/Page404.js';
 import Login from 'Containers/Login/Login';
 import MainDashboard from 'Containers/MainDashboard/MainDashboard.js';
-import Registration from 'Containers/Registration/Registration.js';
 import PrivateRoute from 'HOC/PrivatRoute/PrivatRoute.js';
 import PasswordRecovery from 'Containers/PasswordRecovery/PasswordRecovery';
 import SetNewPassword from 'Containers/SetNewPassword/SetNewPassword';
 import Lesson from 'Containers/Lesson/Lesson';
 import ParentProfile from 'Containers/ParentProfile/ParentProfile';
 import ParentProfileEdit from 'Containers/ParentProfileEdit/ParentProfileEdit';
-import ParentTimeTableSchedule from 'Containers/ParentTimeTableSchedule/ParentTimeTableSchedule';
 import DevicesTestPage from 'Containers/DevicesTestPage/DevicesTestPage';
 import Lessons from 'Containers/Lessons/Lessons';
 import ParentTimeSlots from 'Containers/ParentTimeSlots/ParentTimeSlots';
@@ -18,6 +16,7 @@ import ChildProfile from 'Containers/ChildProfile/ChildProfile';
 import ChildProfiles from 'Containers/ChildProfiles/ChildProfiles';
 
 import './App.css';
+import Registration from 'Containers/Registration/Registration.js';
 
 const App = () => {
 
@@ -33,8 +32,8 @@ const App = () => {
             <PrivateRoute path='/' component={MainDashboard} exact/>
             <PrivateRoute path='/lessons/:lessonId' component={Lesson} />
             <PrivateRoute path='/video-test/' component={DevicesTestPage} />
+            <Route path='/registration/' component={Registration} />
             <Route path='/login/' component={Login}/>
-            <Route path='/registration/:stage/' component={Registration}/>
             <Route path='/reset-password/' component={PasswordRecovery} exact />
             <Route path='/reset-password/:token' component={SetNewPassword} exact />
             <Route component={Page404}/>
