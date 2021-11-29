b86d2b39bdeb2805ba1695199b248f2c841ae2e7
kadyr <kadyrpoyraz@gmail.com>
2021-11-29 23:35:06 +0600

    YB-338 Split Field component

--- src/Components/Field/Field.js
+++ src/Components/Field/Field.js
@@ -1,130 +1,21 @@
 import React from 'react';
 
-import { DesktopDatePicker } from '@mui/lab';
-import { Autocomplete } from '@mui/material';
-import { Controller } from 'react-hook-form'
 import PropTypes from 'prop-types';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import ruLocale from 'date-fns/locale/ru';
 
-import { addClasses } from 'utils/addClasses/addClasses.js';
+import CommonField from 'Components/Fields/CommonField.js';
+import DateField from 'Components/Fields/DateField.js';
+import SelectField from 'Components/Fields/SelectField.js';
 
-const isValidDate = (d) => d instanceof Date && !isNaN(d)
-
-function resolve(path, obj, separator = '.') {
-    var properties = Array.isArray(path) ? path : path.split(separator)
-    return properties.reduce((prev, curr) => prev && prev[curr], obj)
-}
 
 export const Field = (props) => {
 
-    const {
-        id, register, className, errors, label, placeholder, disabled, type, name, control, options
-    } = props
+    const { type } = props
 
-    let field = null
-
-    field = < >
-        <label htmlFor={id} className='form2__label'> {label} < /label>
-        <input
-            type={type || 'text'}
-            className={
-                addClasses(className, {
-                    'error': resolve(name, errors, '.')
-                })
-            }
-
-            id={id}
-            name='first_name' {...register(name)}
-            placeholder={placeholder}
-            disabled={disabled}
-        />
-    </>
-
-    if (type === 'date') {
-        field = <>
-            <label htmlFor='country' className='form2__label'>{label}</label>
-            <Controller
-                name={name}
-                control={control}
-                render={(
-                    { field }
-                ) => {
-                    return (
-                        <LocalizationProvider
-                            dateAdapter={AdapterDateFns}
-                            locale={ruLocale}
-                        >
-                            <DesktopDatePicker
-                                mask='__.__.____'
-                                value={field.value}
-                                onChange={
-                                    (newValue) => field.onChange(isValidDate(newValue) ? newValue : null)}
-                                renderInput={
-                                    (params) => {
-                                        return <input
-                                            {...params.inputProps}
-                                            ref={params.inputRef}
-                                            placeholder='_ _ _'
-                                            className={addClasses(className, {
-                                                'error': resolve(name, errors, '.')
-                                            })}
-                                            id='country'
-                                        />
-                                    }
-                                }
-                            />
-                        </LocalizationProvider>
-                    )
-                }}
-            />
-        </>
+    switch (type) {
+        case 'date': return <DateField {...props} />
+        case 'select': return <SelectField {...props} />
+        default: return <CommonField {...props} />
     }
-
-    if (type === 'select') {
-        field = <>
-            <label htmlFor='country' className='form2__label'>{label}</label>
-            <Controller
-                name={name}
-                control={control}
-                render={(
-                    { field: { onChange, value } }
-                ) => {
-                    return <Autocomplete
-                        name={name}
-                        autoComplete='no'
-                        onChange={(_, { id }) => {
-                            onChange(id)
-                        }}
-                        options={options}
-                        renderInput={(params) => {
-                            params.inputProps.autoComplete = 'new-password';
-                            return (
-                                <div ref={params.InputProps.ref}>
-                                    <input
-                                        value={options?.filter(option => option.value === value)}
-                                        type='text'
-                                        {...params.inputProps}
-                                        className={addClasses(className, {
-                                            'error': resolve(name, errors, '.')
-                                        })}
-                                    />
-                                </div>
-                            )
-                        }}
-                    />
-                }}
-            />
-        </>
-    }
-
-    return (
-        <>
-            {field}
-            <div className='form2__error'> {resolve(name, errors, '.')?.message} < /div>
-        </>
-    )
 }
 
 Field.propTypes = {
