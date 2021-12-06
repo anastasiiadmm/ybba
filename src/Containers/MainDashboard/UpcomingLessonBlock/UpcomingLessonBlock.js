15ee1ce4bdec996285eb90b18c22cef3323d6c66
kadyr <kadyrpoyraz@gmail.com>
2021-12-06 14:35:42 +0600

    YB-373 added child button into UpcomingLessonBlock.js

--- src/Containers/MainDashboard/UpcomingLessonBlock/UpcomingLessonBlock.js
+++ src/Containers/MainDashboard/UpcomingLessonBlock/UpcomingLessonBlock.js
@@ -3,12 +3,14 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
+import ChildCareIcon from '@mui/icons-material/ChildCare';
+import { IconButton } from '@mui/material';
 
 
 const UpcomingLessonBlock = (props) => {
 
     const {
-        lesson
+        lesson, toggleModal
     } = props
 
     const humanize = timestamp => {
@@ -70,6 +72,11 @@ const UpcomingLessonBlock = (props) => {
                             </Link>
                         )
                     )}
+                    <div>
+                        <IconButton onClick={toggleModal}>
+                            <ChildCareIcon fontSize='large'/>
+                        </IconButton>
+                    </div>
                 </div>
             )}
         </>
@@ -86,7 +93,8 @@ UpcomingLessonBlock.propTypes = {
                 date: PropTypes.string
             })
         })
-    }).isRequired
+    }).isRequired,
+    toggleModal: PropTypes.func
 }
 
 export default UpcomingLessonBlock;
