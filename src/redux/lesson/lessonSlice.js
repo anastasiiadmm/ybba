63681ca23b03f7b5c84f72b056dd85985dba6558
Osman Seytnazarov <seytnazarov.o@gmail.com>
2021-12-10 12:36:51 +0600

    Modified components to backend needs, Added fetchLesson, modified GET_LESSON

--- src/redux/lesson/lessonSlice.js
+++ src/redux/lesson/lessonSlice.js
@@ -1,56 +1,79 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axiosApi from '../../axios';
 
 import {
-    GET_LESSON,
-    GET_LESSON_ERROR,
-    CHANGE_ACTIVE_GAME,
-    CHANGE_LESSON_STATUS,
-    RESIZE_CHILD_WEBCAM
+  GET_LESSON,
+  GET_LESSON_ERROR,
+  CHANGE_ACTIVE_GAME,
+  CHANGE_LESSON_STATUS,
+  RESIZE_CHILD_WEBCAM,
 } from 'redux/actionTypes';
 import { LESSON_STATUS_FINISHED } from 'constants.js';
 
-
-const nameSpace = 'lesson'
+const nameSpace = 'lesson';
 
 const INITIAL_STATE = {
-    activeGame: null,
-    lesson: null,
-    error: null,
-    lessonFinished: false,
-    isParentWebcamIncreased: false,
-}
+  activeGame: null,
+  lesson: null,
+  lesson_id: null,
+  error: null,
+  lessonFinished: false,
+  isParentWebcamIncreased: false,
+};
+
+export const fetchLesson = createAsyncThunk(
+  `${nameSpace}/fetchLesson`,
+  async (lessonId, { rejectWithValue }) => {
+    try {
+      const resp = await axiosApi.get(`/lessons/${lessonId}`);
+      return { ...resp.data };
+    } catch (e) {
+      return rejectWithValue(e?.response?.data);
+    }
+  }
+);
 
 const lessonSlice = createSlice({
-    name: nameSpace,
-    initialState: INITIAL_STATE,
-    reducers: {
-        clearLessonState: () => INITIAL_STATE
+  name: nameSpace,
+  initialState: INITIAL_STATE,
+  reducers: {
+    clearLessonState: () => INITIAL_STATE,
+  },
+  extraReducers: {
+    [fetchLesson.pending]: (state) => {
+      //   state.lesson = false;
     },
-    extraReducers: {
-        [GET_LESSON]: (state, { payload }) => {
-            state.lesson = payload.lesson
-        },
-        [GET_LESSON_ERROR]: (state, { payload }) => {
-            state.error = payload.message
-        },
-
-        [CHANGE_ACTIVE_GAME]: (state, { payload }) => {
-            state.lesson = payload.lesson
-        },
-
-        [CHANGE_LESSON_STATUS]: (state, { payload }) => {
-            if (payload.status === LESSON_STATUS_FINISHED) {
-                state.lesson = null
-                state.lessonFinished = true
-            }
-        },
-        [RESIZE_CHILD_WEBCAM]: (state, { payload }) => {
-            state.isParentWebcamIncreased = payload
-        }
+    [fetchLesson.fulfilled]: (state, action) => {
+      state.lesson = action.payload;
     },
-})
+    [fetchLesson.rejected]: (state, { payload }) => {
+      //   state.error = payload;
+      //   state.lesson = false;
+    },
 
+    [GET_LESSON]: (state, { payload }) => {
+      state.lesson_id = { payload };
+    },
+    [GET_LESSON_ERROR]: (state, { payload }) => {
+      state.error = payload.message;
+    },
 
-export const { clearLessonState } = lessonSlice.actions
-export const lessonSelector = state => state.lesson
-export default lessonSlice.reducer
+    [CHANGE_ACTIVE_GAME]: (state, { payload }) => {
+      state.lesson = payload.lesson;
+    },
+
+    [CHANGE_LESSON_STATUS]: (state, { payload }) => {
+      if (payload.status === LESSON_STATUS_FINISHED) {
+        state.lesson = null;
+        state.lessonFinished = true;
+      }
+    },
+    [RESIZE_CHILD_WEBCAM]: (state, { payload }) => {
+      state.isParentWebcamIncreased = payload;
+    },
+  },
+});
+
+export const { clearLessonState } = lessonSlice.actions;
+export const lessonSelector = (state) => state.lesson;
+export default lessonSlice.reducer;
