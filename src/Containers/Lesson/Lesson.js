1743f3931f9151364f89f379de9cce0fe66e02a7
Osman Seytnazarov <seytnazarov.o@gmail.com>
2021-12-10 16:10:42 +0600

    removed old code

--- src/Containers/Lesson/Lesson.js
+++ src/Containers/Lesson/Lesson.js
@@ -3,14 +3,9 @@ import React, { useEffect, useState } from 'react';
 import WebSocket from 'Containers/WebSocket/WebSocket.js';
 import LessonPage from 'Containers/LessonPage/LessonPage.js';
 import { getLesson, resizeChildWebcam } from 'redux/lesson/actions.js';
-import { useDispatch } from 'react-redux';
-import { fetchLesson } from 'redux/lesson/lessonSlice';
 
 const Lesson = (props) => {
   const { lessonId } = props.match.params;
-  const dispatch = useDispatch();
-  dispatch(fetchLesson(lessonId));
-  //   console.log(testFetch);
   const initMessages = [
     getLesson({ lesson_id: lessonId }),
     resizeChildWebcam({
