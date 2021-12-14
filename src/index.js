1afc8e0d569fc8021d93ad19e1395a40dcd55168
anastasiiapanama <kilma.1334@gmail.com>
2021-12-14 09:54:34 +0600

    YB-395 comment browser permissions block

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import configureStore from 'redux/store.js'
 import reportWebVitals from 'reportWebVitals';
 import { initSentry } from 'sentry/sentry';
 import App from 'App.js';
-import BrowserPermissionsBlock from 'Containers/BrowserPermissionsBlock/BrowserPermissionsBlock';
 
 import 'assets/vendor/flatpickr/flatpickr'
 import 'assets/vendor/flatpickr/ru'
@@ -25,13 +24,13 @@ export const store = configureStore()
 ReactDOM.render(
     <React.StrictMode>
         <FirebaseBlock>
-            <BrowserPermissionsBlock>
+            {/*<BrowserPermissionsBlock>*/}
                 <Provider store={store}>
                     <BrowserRouter>
                         <App/>
                     </BrowserRouter>
                 </Provider>
-            </BrowserPermissionsBlock>
+            {/*</BrowserPermissionsBlock>*/}
         </FirebaseBlock>
     </React.StrictMode>,
     document.getElementById('root')
