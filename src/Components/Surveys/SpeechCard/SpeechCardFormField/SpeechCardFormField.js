7c4cafe8f3970f3d0fb2437ce0d97daa56cdacb5
kadyr <kadyrpoyraz@gmail.com>
2022-02-01 11:41:45 +0600

    YB-492: Added onfocus handler for speech card form field

--- src/Components/Surveys/SpeechCard/SpeechCardFormField/SpeechCardFormField.js
+++ src/Components/Surveys/SpeechCard/SpeechCardFormField/SpeechCardFormField.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Field from 'Components/Field/Field.js';
+import { relationOfProtocolToSpeechCard } from 'constants.js';
 
 const SpeechCardFormField = (props) => {
 
@@ -8,11 +9,21 @@ const SpeechCardFormField = (props) => {
         type
     } = props
 
+    const inputFocusHandler = e => {
+        const name = relationOfProtocolToSpeechCard[e.target.name]
+        const element = document.querySelector(`[name="${name}"].protocolField`)
+
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' })
+        }
+    }
+
     switch (type) {
         case 'textarea': return <Field
             className='speech-card-form__area'
             classNameLabel='speech-card-form__lbl'
             id={props.name}
+            onFocus={inputFocusHandler}
             {...props}
         />
         case 'number':
@@ -20,11 +31,13 @@ const SpeechCardFormField = (props) => {
             className='speech-card-form__field'
             classNameLabel='speech-card-form__lbl'
             id={props.name}
+            onFocus={inputFocusHandler}
             {...props}
         />
         case 'radio': return <Field
             value={props.label.toLocaleLowerCase()}
             classNameLabel='radio2'
+            onFocus={inputFocusHandler}
             {...props}
         />
         default: return <p>Field</p>
@@ -45,7 +58,8 @@ SpeechCardFormField.propTypes = {
     errors: PropTypes.object,
     value: PropTypes.any,
     control: PropTypes.object,
-    disabled: PropTypes.bool
+    disabled: PropTypes.bool,
+    onClick: PropTypes.func,
 }
 
 export default SpeechCardFormField;
