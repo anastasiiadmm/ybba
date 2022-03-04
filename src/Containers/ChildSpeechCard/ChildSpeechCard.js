69f7482503a8f904ce02e03c7a2b395bd7fd7326
cher.alexandr <cher.alexandr@gmail.com>
2022-03-04 19:50:35 +0600

    YB-595 - Changed the logo, corrected the styles of the pdf and the speech card.

--- src/Containers/ChildSpeechCard/ChildSpeechCard.js
+++ src/Containers/ChildSpeechCard/ChildSpeechCard.js
@@ -1,10 +1,10 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import MainTitleBlock from 'Containers/MainDashboard/MainTitleBlock/MainTitleBlock.js';
 import SidebarContainer from 'Components/SidebarContainer/SidebarContainer.js';
 import SpeechCardView from 'Containers/Surveys/SpeechCardView/SpeechCardView.js';
 import { useReactToPrint } from 'react-to-print';
 import { useDispatch, useSelector } from 'react-redux';
-import { getSpeechCard, surveysSelector, clearSurveysState } from 'redux/surveys/surveysSlice.js';
+import { clearSurveysState, getSpeechCard, surveysSelector } from 'redux/surveys/surveysSlice.js';
 import { Spinner } from 'react-bootstrap';
 
 import 'Containers/ChildSpeechCard/childSpeechCard.css'
@@ -13,53 +13,53 @@ import { useHistory } from 'react-router';
 
 const ChildSpeechCard = (props) => {
 
-    const { childId } = props.match.params
+  const { childId } = props.match.params
 
-    const speechCardRef = useRef(null)
-    const dispatch = useDispatch()
-    const history = useHistory()
+  const speechCardRef = useRef(null)
+  const dispatch = useDispatch()
+  const history = useHistory()
 
-    const { speechCard } = useSelector(surveysSelector)
+  const { speechCard } = useSelector(surveysSelector)
 
-    const handlePrint = useReactToPrint({
-        content: () => speechCardRef.current
-    })
+  const handlePrint = useReactToPrint({
+    content: () => speechCardRef.current
+  })
 
-    useEffect(() => {
-        dispatch(getSpeechCard(childId))
-    }, [childId, dispatch])
+  useEffect(() => {
+    dispatch(getSpeechCard(childId))
+  }, [childId, dispatch])
 
-    useEffect(() => {
-        if (speechCard && !speechCard.is_approved) {
-            dispatch(clearSurveysState())
-            history.push('/child-profile/')
-            toast.warning('Речевая карта ещё не заполнена.')
-        }
-    }, [dispatch, history, speechCard])
+  useEffect(() => {
+    if (speechCard && !speechCard.is_approved) {
+      dispatch(clearSurveysState())
+      history.push('/child-profile/')
+      toast.warning('Речевая карта ещё не заполнена.')
+    }
+  }, [dispatch, history, speechCard])
 
-    return (
-        <SidebarContainer>
-            <MainTitleBlock
-                leftTitle='Речевая карта'
-                rightTitle={
-                    <button
-                        className='link-download border-0'
-                        onClick={handlePrint}
-                    >Скачать</button>
-                }
+  return (
+      <SidebarContainer>
+        <MainTitleBlock
+            leftTitle='Речевая карта'
+            rightTitle={
+              <button
+                  className='link-download border-0'
+                  onClick={handlePrint}
+              >Скачать</button>
+            }
+        />
+        {speechCard && speechCard.is_approved ? (
+            <SpeechCardView
+                ref={speechCardRef}
+                speechCard={speechCard}
             />
-            {speechCard && speechCard.is_approved ? (
-                <SpeechCardView
-                    ref={speechCardRef}
-                    speechCard={speechCard}
-                />
-            ) : (
-                <div className='w-100 h-100 d-flex align-items-center justify-content-center'>
-                    <Spinner animation='grow'/>
-                </div>
-            )}
-        </SidebarContainer>
-    );
+        ) : (
+            <div className='w-100 h-100 d-flex align-items-center justify-content-center'>
+              <Spinner animation='grow'/>
+            </div>
+        )}
+      </SidebarContainer>
+  );
 };
 
 export default ChildSpeechCard;
